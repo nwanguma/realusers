a2be40aa78a98bc085ddd4118c0a3c0521b18334
Tochukwu Nwanguma <tochukwu@cico.ng>
2020-12-18 17:23:24 +0100

    Move reusable components to new file
    Create and style register page

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,5 +1,69 @@
-const Register = () => {
-  return <div>this is the Register page</div>;
+import styled from "styled-components";
+
+import {
+  PageWrapper,
+  FormWrapper,
+  FormDetails,
+  FormDetailsLogo,
+  FormDetailsTitle,
+  LoginAlt,
+  LoginAltLogo,
+  LoginAltText,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Submit,
+  RegisterLink,
+} from "../components/styled/onboarding";
+
+import logo from "../assets/images/logo.png";
+import google from "../assets/images/google.svg";
+
+const RegisterForm = styled(Form)`
+  height: 38.5rem;
+`;
+
+const LoginLink = styled(RegisterLink)`
+  display: block;
+  text-decoration: none;
+  font-size: 1.4rem;
+  font-weight: 600;
+  color: #312aaa;
+  text-align: center;
+`;
+
+const RegisterPage = () => {
+  return (
+    <PageWrapper>
+      <FormWrapper>
+        <FormDetails>
+          <FormDetailsLogo src={logo} alt="" />
+          <FormDetailsTitle>Register to start earning</FormDetailsTitle>
+        </FormDetails>
+        <LoginAlt href="#">
+          <LoginAltLogo src={google} alt="" />
+          <LoginAltText>Sign up with Google</LoginAltText>
+        </LoginAlt>
+        <RegisterForm>
+          <FormGroup>
+            <Label>Email</Label>
+            <Input type="text" autoComplete="new-password" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Date of birth</Label>
+            <Input type="date" autoComplete="new-password" />
+          </FormGroup>
+          <FormGroup>
+            <Label>Password</Label>
+            <Input type="password" autoComplete="new-password" />
+          </FormGroup>
+          <Submit>Submit</Submit>
+          <LoginLink to="/login">Already registered? Login</LoginLink>
+        </RegisterForm>
+      </FormWrapper>
+    </PageWrapper>
+  );
 };
 
-export default Register;
+export default RegisterPage;
