399c679b2f7dc1a1eeaedba34aa3353766ff7480
Tochukwu Nwanguma <tochukwu@cico.ng>
2020-12-18 16:23:43 +0100

    Create login page
    Style login page

---
+++ src/pages/Register.js
@@ -0,0 +1,5 @@
+const Register = () => {
+  return <div>this is the Register page</div>;
+};
+
+export default Register;
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,20 +1,25 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "../pages/landing/Home";
+// import Home from "../pages/landing/Home";
 import Promotions from "../pages/landing/Promotions";
 import About from "../pages/landing/About";
 import Product from "../pages/landing/Product";
 import Pricing from "../pages/landing/Pricing";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
         <Route path="/pricing" component={Pricing} />
         <Route path="/promotions" component={Promotions} />
         <Route path="/about" component={About} />
         <Route path="/product" component={Product} />
-        <Route path="/*" component={Home} />
+        <Route path="/*" component={Login} />
+        <Route path="/" exact component={Login} />
       </Switch>
     </Router>
   );
